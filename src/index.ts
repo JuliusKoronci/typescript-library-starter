a40d6644c6f901498db5dca97dc0e45c259a9167
Julius Koronci <julius.koronci@external.erstegroup.com>
2019-10-14 14:24:56 +0200

    feat: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function component(): HTMLDivElement {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = ["Hello", "webpack"].join(" ");
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader'
+        },
+      },
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'awesome-typescript-loader'
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new CheckerPlugin()
+  ]
+};
